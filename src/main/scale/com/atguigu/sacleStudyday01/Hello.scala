4c9119e97ab0478ea2b12779916ff868848f4462
jiazhaoquan <jzq17856170605@163.com>
2021-04-07 01:39:27 +0800

    ceshi

--- src/main/scale/com/atguigu/sacleStudyday01/Hello.scala
+++ src/main/scale/com/atguigu/sacleStudyday01/Hello.scala
@@ -3,6 +3,9 @@ package com.atguigu.sacleStudyday01
 object Hello {
   def main(args: Array[String]): Unit = {
     println("hello,scale")
+    println("hello,scale")
+    println("hello,scale")
+    println("hello,scale")
   }
 
 }
