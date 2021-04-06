5d339e118c57feec9ca89e044f6999c5a4043d4a
jiazhaoquan <jzq17856170605@163.com>
2021-04-07 01:20:04 +0800

    ceshi

---
+++ src/main/scale/com/atguigu/sacleStudyday01/Hello.scala
@@ -0,0 +1,8 @@
+package com.atguigu.sacleStudyday01
+
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println("hello,scale")
+  }
+
+}
---
+++ src/main/scale/com/atguigu/sacleStudyday01/printDemo.scala
@@ -0,0 +1,18 @@
+package com.atguigu.sacleStudyday01
+
+object printDemo {
+  def main(args: Array[String]): Unit = {
+    var str1 = "hello"
+    var str2= "hello"
+    //println(str1+str2)
+    var name:String = "tom"
+    var age:Int = 10
+    var sal:Float = 10.67f
+    var height:Double = 180.15
+    printf("名字=%s 年龄是%d 薪水是%.2f 升高%.3f",name,age,sal,height)
+
+    println(s"个人信息如下：\n 名字$name \n 年龄$age \n 薪水${sal*10}"  )
+
+  }
+
+}
